b2aedab24e6ec448c42ae5c5f18b139a20725e74
descawed <github@descawed.com>
2026-08-01 15:01:04 -0400

    Make sure the patch macro always uses the top-level hook86 crate

--- hook86_macro/src/lib.rs
+++ hook86_macro/src/lib.rs
@@ -226,7 +226,7 @@ pub fn patch(input: TokenStream) -> TokenStream {
 
             pub unsafe fn bind(&mut self, #(#field_names: hook86::mem::IntPtr,)*) -> windows::core::Result<*const u8> {
                 #(self.#field_names.set_value(&mut self.__buf, #field_names);)*
-                unsafe { hook86::mem::unprotect(self.buf_raw() as *const std::ffi::c_void, #patch_size).map(|_| self.buf_raw()) }
+                unsafe { ::hook86::mem::unprotect(self.buf_raw() as *const std::ffi::c_void, #patch_size).map(|_| self.buf_raw()) }
             }
         }
     };
